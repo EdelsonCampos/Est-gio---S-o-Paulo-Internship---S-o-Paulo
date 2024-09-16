41a8534dfe34334d639b186fbbb55a35bddc2c8f
Edelson Campos <edelsonfilho269@gmail.com>
2024-09-16 17:14:20 -0300

    commit full course

---
+++ Atividade-SP/DesafioHome/scripts/main.js
@@ -0,0 +1,11 @@
+import sumResult from './sumResult.js';
+import isFibonacci from './isFibonacci.js';
+import billStatus from './billStatus.js';
+import stateShare from './stateShare.js';
+import reverseWord from './reverseWord.js';
+
+sumResult();
+isFibonacci();
+billStatus();
+stateShare();
+reverseWord();
